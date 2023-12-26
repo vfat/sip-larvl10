04aa790b83f2f2ee7b578bb9b4c304f3c5a88c32
Vicky Fatrian <vickyfatrian.ayomama@gmail.com>
2023-12-26 22:18:23 +0700

    20231226

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            BbuSeeder::class,
+            PbuSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
