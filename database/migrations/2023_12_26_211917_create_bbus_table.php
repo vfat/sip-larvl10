fcb301c6634492608cd6a7a660e3a54539ed5e23
Vicky Fatrian <vickyfatrian.ayomama@gmail.com>
2023-12-26 21:54:09 +0700

    20231226

---
+++ database/migrations/2023_12_26_211917_create_bbus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bbus', function (Blueprint $table) {
+            $table->id();
+            $table->string('umur')->nullable();
+            $table->string('min_3_sd')->nullable();
+            $table->string('min_2_sd')->nullable();
+            $table->string('min_1_sd')->nullable();
+            $table->string('median')->nullable();
+            $table->string('plus_1_sd')->nullable();
+            $table->string('plus_2_sd')->nullable();
+            $table->string('plus_3_sd')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bbus');
+    }
+};
